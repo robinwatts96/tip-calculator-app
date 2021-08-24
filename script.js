b0302eea0eea896dd5b79733c1a397ffbf5c2c0c
robinwatts96 <robinwatts96@gmail.com>
2021-08-24 08:00:54 +0100

    Logic plan added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert('hi');
+// get bill
+// get chosen tip
+// get number of people
+// add chosen peecentage to bill
+// get total, and divide by num of people
+// add this to total p personal
+
+// total x tip amount - to get the value of all peoples tips
+// / num of people 
+// return tip per person to tip amout per person
+
+// if num of people =<0 add html 'can't be zero/add orange border to num of people input
+
+// reset button to return all values to zero
