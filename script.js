9f1522db9906858ada6c8e9b1ab5bbd49ebc9824
robinwatts96 <robinwatts96@gmail.com>
2021-09-02 08:03:22 +0100

    Jquery added

--- script.js
+++ script.js
@@ -15,34 +15,41 @@
 // reset button to return all values to zero
 
 
-// Variables
-let bill = document.getElementById('bill-input').value;
-console.log('bill: ' + bill);
+// // Variables
+// let bill = document.getElementById('bill-input').value;
+// console.log('bill: ' + bill);
 
 
 
-const tip5 = document.getElementById('tip5').value;
-const tip10 = document.getElementById('tip10').value;
-const tip15 = document.getElementById('tip15').value;
-const tip20 = document.getElementById('tip20').value;
-const tip25 = document.getElementById('tip25').value;
-const tipCustom = document.getElementById('custom').value;
+// const tip5 = document.getElementById('tip5').value;
+// const tip10 = document.getElementById('tip10').value;
+// const tip15 = document.getElementById('tip15').value;
+// const tip20 = document.getElementById('tip20').value;
+// const tip25 = document.getElementById('tip25').value;
+// const tipCustom = document.getElementById('custom').value;
 
-let tip = document.getElementsByClassName('btn-tip').value;
+// // let tip = document.getElementsByClassName('btn-tip').value;
 
-const nop = document.getElementById('numOfPeople').value;
+// const nop = document.getElementById('numOfPeople').value;
 
-console.log(tip5.value);
-console.log(tipCustom.value);
+// // console.log(tip5.value);
+// // console.log(tipCustom.value);
 
-// const tip5 = function () {
-//     document.getElementById('tip5');
-//     document.addEventListener('onClick')
-// }
+// // const tip5 = function () {
+// //     document.getElementById('tip5');
+// //     document.addEventListener('onClick')
+// // }
 
-document.querySelector('.btn-tip').addEventListener('click', function calculate () {
-    document.querySelector('body').style.background = "red";
-});
+// document.querySelector('.btn-tip').currentTarget.addEventListener('click', function calculate () {
+//     let tipValue = document.querySelector('.btn-tip').currentTarget.value;
+//     console.log(tipValue);
+// });
+
+
+$(document).ready(function() {
+    // This code only runs after the DOM is loaded.
+    alert('DOM fully loaded!');
+  });
 
 
 
