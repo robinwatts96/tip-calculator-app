fa602f8ff5b6f2d9219b0950be5129e6cdcc060f
robinwatts96 <robinwatts96@gmail.com>
2021-09-07 08:19:06 +0100

    See notes in script

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-// get bill
-// get chosen tip
-// get custom tip
-// get number of people
-// add chosen percentage to bill
-// get total, and divide by num of people
-// add this to total p personal
-
-// total x tip amount - to get the value of all peoples tips
-// / num of people 
-// return tip per person to tip amout per person
-
-// if num of people =<0 add html 'can't be zero/add orange border to num of people input
-
-// reset button to return all values to zero
-
-
-
-
-// Click tip btn
+// Click tip btn to calculate
 $('.btn-tip').on('click', event => {
     let tip = $(event.currentTarget).val();
     // console.log('You selected: ' + tip);
